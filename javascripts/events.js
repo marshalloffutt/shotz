d046bd8a03fb4c68040c4171aa934a3ab033fec3
Marshall Offutt <marshalloffutt@gmail.com>
2018-10-19 22:49:38 -0500

    Adds keypress event for searchbar, but its broken

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,1 +1,8 @@
-console.log('events');
+const locationSearch = () => {
+    $("#searchBar").keypress(function() {
+        let searchTerm = $("#searchBar").val().toLowerCase;
+        $('.locations').not(':contains(searchTerm).toggle()')
+    });
+};
+
+export { locationSearch }
