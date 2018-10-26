dc0d53fa04c8d4604094c3ea869bd73debad6076
Marshall Offutt <marshalloffutt@gmail.com>
2018-10-25 20:13:30 -0500

    Clicking on a movie card does a thing

--- javascripts/components/movieComponent.js
+++ javascripts/components/movieComponent.js
@@ -1,11 +1,20 @@
 import { loadMovies } from "../data/movieData.js";
 
+// Clicking on a movie card does something
+const mainViewEvents = () => {
+    $('#movies').on('click', '.movie-card', (e) => {
+        const movieCardIClicked = $(e.target).closest('.movie-card').attr('id');
+        $('#main-view').hide();
+        $('#secondary-view').show();
+    })
+}
+
 // Add movie info to dom
 const writeMovies = (movies) => {
     let domString = '';
     movies.forEach((movie) => {
         domString += `
-        <div id ="${movie.id}" class="card m-4" style="width: 18rem;">
+        <div id ="${movie.id}" class="card movie-card m-4" style="width: 18rem;">
             <div class="card-body">
                 <h5 class="card-title">${movie.name}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">${movie.genre}</h6>
@@ -22,6 +31,7 @@ const writeMovies = (movies) => {
 const initializeMainView = () => {
     loadMovies().then((movies) => {
         writeMovies(movies);
+        mainViewEvents();
     })
 }
 
